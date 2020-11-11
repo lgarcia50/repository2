77723b5ebc5771c67904d7d2b39ca78cd1751e40
Lucia <lgarcia@eepmad.com>
2020-11-11 17:33:08 +0100

    hola

--- Programacion/src/ut3/Holaclase.java
+++ Programacion/src/ut3/Holaclase.java
@@ -8,13 +8,20 @@ public class Holaclase {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int clase;
+		int salir =0;
+		while (true) {
 	    System.out.println("Cuantos somos en clase:");
 	    clase = sc.nextInt();
 		System.out.println("Hola clase de primero de informatica otra vez");
 		System.out.println("Hola clase, esto es un cambio para guardar nueva version");
 		
 		System.out.println("Otro cambio");
+		 System.out.println("Si quiere poner otros datos pulse(1), si quiere salir pulse(0)");
+		 salir = sc.nextInt();
+		while(salir==0);
+		 
+		
 
 	}
-
+	}
 }

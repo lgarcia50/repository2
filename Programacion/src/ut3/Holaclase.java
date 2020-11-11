054ea457e173d3b2a639199ed6f2a5720ac37b7e
Lucia <lgarcia@eepmad.com>
2020-11-11 16:27:52 +0100

    holaclase

--- Programacion/src/ut3/Holaclase.java
+++ Programacion/src/ut3/Holaclase.java
@@ -1,9 +1,15 @@
 package ut3;
 
+import java.util.Scanner;
+
 public class Holaclase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int clase;
+	    System.out.println("Cuantos somos en clase:");
+	    clase = sc.nextInt();
 		System.out.println("Hola clase de primero de informatica otra vez");
 		System.out.println("Hola clase, esto es un cambio para kguardar nueva version");
 

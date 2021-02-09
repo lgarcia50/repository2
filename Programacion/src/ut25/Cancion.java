ea60a723d54b2e40c189b3a3f3c1de02211f2897
Lucia <lgarcia@eepmad.com>
2021-02-09 12:18:26 +0100

    ejercicio25

--- Programacion/src/ut25/Cancion.java
+++ Programacion/src/ut25/Cancion.java
@@ -10,7 +10,7 @@ package ut25;
 public class Cancion {
 	private String titulo;
 	private String artista;
-	private double duracion;
+	private int duracion;
 	
 	/**
 	 * Constructor vacio
@@ -22,24 +22,24 @@ public class Cancion {
 	 * @param artista Nombre del artista 
 	 * @param duracion Duracion de la cancion 
 	 */
-	public Cancion(String titulo, String artista, double duracion) {
+	public Cancion(String titulo, String artista, int d) {
 		this.titulo=titulo;
 		this.artista=artista;
-		this.duracion=duracion;
+		this.duracion= d;
 	}
 	/**
 	 * Metodo reproducir
 	 * @return Reproduciendo titulo de la cancion
 	 */
 	public String reproducir() {
-		return "Reproduciendo..."+this.titulo;
+		return "Reproduciendo"+getTitulo();
 	}
 	@Override
 	/**
 	 * Este metodo nos va a devolver el titulo de la cancion ,el artista y la duracion de una cancion 
 	 */
 	public String toString() {
-		return this.titulo+" - "+this.artista+"| Duracion:"+this.duracion+", minutos";
+		return ""+getTitulo()+"-"+getArtista()+"("+getDuracion()+" minutos)";
 	}
 
 	/**
@@ -47,27 +47,31 @@ public class Cancion {
 	 * @return
 	 */
 	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getArtista() {
-		return artista;
-	}
-
-	public void setArtista(String artista) {
-		this.artista = artista;
-	}
-
-	public double getDuracion() {
-		return duracion;
-	}
-
-	public void setDuracion(float duracion) {
-		this.duracion = duracion;
-	}
-	
+	return titulo;
+}
+public String getArtista() {
+	return artista;
+}
+public int getDuracion() {
+	return duracion;
+}
+//Setters privado para todos los atributos.
+public void setTitulo(String t) {
+	if (t.equals(""))
+		this.titulo= "Sin titulo";
+	else 
+		this.titulo=t;
+}
+public void setArtista(String a) {
+	if (a.equals(""))
+		this.artista= "Sin artista";
+	else 
+		this.artista=a;
+}
+public void setDuracionMinutos(int d) {
+	if (d<0)
+		this.duracion= 0;
+	else 
+		this.duracion=d;
+}
 }

31194711a727b538c7e887bc081b0d9c7b826e5c
Lucia <lgarcia@eepmad.com>
2021-02-09 10:51:27 +0100

    Ejercicio25

---
+++ Programacion/src/ut25/Cancion.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package ut25;
+
+/**
+ * @author lucia
+ *
+ */
+public class Cancion {
+	private String titulo;
+	private String artista;
+	private double duracion;
+	
+	/**
+	 * Constructor vacio
+	 */
+	public Cancion() {}
+	/**
+	 * Constructor con parametos
+	 * @param titulo Nombre de la cancion 
+	 * @param artista Nombre del artista 
+	 * @param duracion Duracion de la cancion 
+	 */
+	public Cancion(String titulo, String artista, double duracion) {
+		this.titulo=titulo;
+		this.artista=artista;
+		this.duracion=duracion;
+	}
+	/**
+	 * Metodo reproducir
+	 * @return Reproduciendo titulo de la cancion
+	 */
+	public String reproducir() {
+		return "Reproduciendo..."+this.titulo;
+	}
+	@Override
+	/**
+	 * Este metodo nos va a devolver el titulo de la cancion ,el artista y la duracion de una cancion 
+	 */
+	public String toString() {
+		return this.titulo+" - "+this.artista+"| Duracion:"+this.duracion+", minutos";
+	}
+
+	/**
+	 * Getters y setters
+	 * @return
+	 */
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getArtista() {
+		return artista;
+	}
+
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+
+	public double getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(float duracion) {
+		this.duracion = duracion;
+	}
+	
+}
---
+++ Programacion/src/ut25/Playlist.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ut25;
+import java.util.ArrayList;
+/**
+ * @author lucia
+ *
+ */
+
+	public class Playlist extends Cancion{
+		/**
+		 * ArrayList llamado playlist para ir metiendo canciones alli;
+		 */
+		ArrayList<Cancion> playlist = new ArrayList();
+		public void aniadirCancion() {
+			
+		}
+	}
+	
